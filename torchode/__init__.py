6a4cc529dddc11d79e4363518e56153a2fd41069
Marten Lienen <m.lienen@tum.de>
2022-11-16 21:49:21 +0100

    Bump version to 0.1.1

--- torchode/__init__.py
+++ torchode/__init__.py
@@ -1,6 +1,6 @@
 """A parallel ODE solver for PyTorch"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .adjoints import AutoDiffAdjoint, BacksolveAdjoint, JointBacksolveAdjoint
 from .interface import register_method, solve_ivp
